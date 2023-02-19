cd2534565f0b5b82286e359c2e23cdb0b0b76b3b
Anastasia Bodnyuk <www.nastya97@yandex.ru>
2023-02-19 19:33:07 +0300

    feat: final changes after review

--- src/components/App.js
+++ src/components/App.js
@@ -44,102 +44,100 @@ function App() {
 
   return (
     <div className="App">
-      <body className="root">
-        <div className="page">
-          <Header />
-          <Main
-            onEditProfile={handleEditProfileClick}
-            onAddPlace={handleAddPlaceClick}
-            onEditAvatar={handleEditAvatarClick}
-            onCardClick={handleCardClick}
+      <div className="page">
+        <Header />
+        <Main
+          onEditProfile={handleEditProfileClick}
+          onAddPlace={handleAddPlaceClick}
+          onEditAvatar={handleEditAvatarClick}
+          onCardClick={handleCardClick}
+        />
+        <Footer />
+        <PopupWithForm
+          title="Редактировать профиль"
+          name="edit-button"
+          btnText="Сохранить"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <input
+            id="firstname-input"
+            type="text"
+            name="name"
+            placeholder="Имя пользователя"
+            className="form__item form__item_info_name"
+            minLength="2"
+            maxLength="40"
+            required
           />
-          <Footer />
-          <PopupWithForm
-            title="Редактировать профиль"
-            name="edit-button"
-            btnText="Сохранить"
-            isOpen={isEditProfilePopupOpen}
-            onClose={closeAllPopups}
-          >
-            <input
-              id="firstname-input"
-              type="text"
-              name="name"
-              placeholder="Имя пользователя"
-              className="form__item form__item_info_name"
-              minLength="2"
-              maxLength="40"
-              required
-            />
-            <span className="firstname-input-error form__item-error"></span>
-            <input
-              id="career-input"
-              type="text"
-              name="about"
-              placeholder="Род деятельности"
-              className="form__item form__item_info_job"
-              minLength="2"
-              maxLength="200"
-              required
-            />
-            <span className="career-input-error form__item-error"></span>
-          </PopupWithForm>
-
-          <PopupWithForm
-            name="add-photo"
-            title="Новое место"
-            btnText="Создать"
-            isOpen={isAddPlacePopupOpen}
-            onClose={closeAllPopups}
-          >
-            <input
-              id="title-input"
-              type="text"
-              name="name"
-              placeholder="Название"
-              className="form__item form__item_info_title"
-              minLength="2"
-              maxLength="30"
-              required
-            />
-            <span className="title-input-error form__item-error"></span>
-            <input
-              id="link-input"
-              type="url"
-              name="link"
-              placeholder="Ссылка на картинку"
-              className="form__item form__item_info_link"
-              required
-            />
-            <span className="link-input-error form__item-error"></span>
-          </PopupWithForm>
-
-          <ImagePopup
-            isOpen={isCardPopupOpen}
-            onClose={closeAllPopups}
-            card={selectedCard}
+          <span className="firstname-input-error form__item-error"></span>
+          <input
+            id="career-input"
+            type="text"
+            name="about"
+            placeholder="Род деятельности"
+            className="form__item form__item_info_job"
+            minLength="2"
+            maxLength="200"
+            required
           />
-          <PopupWithForm
-            name="edit-avatar"
-            title="Обновить аватар"
-            btnText="Создать"
-            isOpen={isEditAvatarPopupOpen}
-            onClose={closeAllPopups}
-          >
-            <input
-              id="avatar-input"
-              type="url"
-              name="avatar"
-              placeholder="Ссылка на картинку"
-              className="form__item form__item_avatar_link"
-              required
-            />
-            <span className="avatar-input-error form__item-error"></span>
-          </PopupWithForm>
+          <span className="career-input-error form__item-error"></span>
+        </PopupWithForm>
 
-          <PopupWithForm name="delete" title="Вы уверены?" btnText="Да" />
-        </div>
-      </body>
+        <PopupWithForm
+          name="add-photo"
+          title="Новое место"
+          btnText="Создать"
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}
+        >
+          <input
+            id="title-input"
+            type="text"
+            name="name"
+            placeholder="Название"
+            className="form__item form__item_info_title"
+            minLength="2"
+            maxLength="30"
+            required
+          />
+          <span className="title-input-error form__item-error"></span>
+          <input
+            id="link-input"
+            type="url"
+            name="link"
+            placeholder="Ссылка на картинку"
+            className="form__item form__item_info_link"
+            required
+          />
+          <span className="link-input-error form__item-error"></span>
+        </PopupWithForm>
+
+        <ImagePopup
+          isOpen={isCardPopupOpen}
+          onClose={closeAllPopups}
+          card={selectedCard}
+        />
+        <PopupWithForm
+          name="edit-avatar"
+          title="Обновить аватар"
+          btnText="Создать"
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}
+        >
+          <input
+            id="avatar-input"
+            type="url"
+            name="avatar"
+            placeholder="Ссылка на картинку"
+            className="form__item form__item_avatar_link"
+            required
+          />
+          <span className="avatar-input-error form__item-error"></span>
+        </PopupWithForm>
+
+        <PopupWithForm name="delete" title="Вы уверены?" btnText="Да" />
+      </div>
     </div>
   );
 }
